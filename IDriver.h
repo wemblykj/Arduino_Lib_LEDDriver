4f76450b3e0973218be9d644f1a0f5a61b7e7baf
Paul Wightmore <paul.wightmore@gmail.com>
2015-11-18 17:35:43 +0000

    sketching out library for RGB support

---
+++ IDriver.h
@@ -0,0 +1,12 @@
+/* IDriver interface
+ * Paul Wightmore 2015
+ */
+#ifndef _IDRIVER_h
+#define _IDRIVER_h
+
+class IDriver
+{
+  virtual void reset() = 0;
+};
+
+#endif // _IDRIVER_h
---
+++ common.h
@@ -0,0 +1,2 @@
+// Used to identify a class function as overriden from a base class
+#define override
